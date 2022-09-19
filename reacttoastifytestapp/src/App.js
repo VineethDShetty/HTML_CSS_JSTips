b350b4e590789978a5c28c952ae6c9bcd2eedd18
VineethDShetty <vineethshetty289@gmail.com>
2022-09-19 21:03:39 +0530

    React Toastify Push

---
+++ reacttoastifytestapp/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  const notify = () => toast("Wow so easy!");
+  return (
+    <div className="App">
+     <button className="Notifybuttton" onClick={notify}>Notify!</button>
+     <ToastContainer
+      
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      toastStyle={{ backgroundColor: "#332321",color:'greenyellow' }}
+/>
+    </div>
+  );
+}
+
+export default App;
